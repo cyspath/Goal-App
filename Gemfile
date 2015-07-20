2126ca4c9afbd1fb86e1bd68822924d447d0359c
Mike Li <mzl@berkeley.edu>
2015-07-20 11:58:31 -0700

    Add auth specs

--- Gemfile
+++ Gemfile
@@ -42,13 +42,17 @@ group :development, :test do
   gem 'faker'
   gem 'capybara'
   gem 'pry-rails'
-  gem 'binding_of_caller'
-  gem 'better_errors'
-  gem 'bcrypt'
 
-  # Access an IRB console on exception pages or by using <%= console %> in views
-  gem 'web-console', '~> 2.0'
+  gem 'bcrypt'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+end
+
+group :development do
+  # Access an IRB console on exception pages or by using <%= console %> in views
+  gem 'web-console', '~> 2.0'
+
+  gem 'binding_of_caller'
+  gem 'better_errors'
 end
---
+++ spec/feature/auth_spec.rb
@@ -0,0 +1,88 @@
+require 'spec_helper'
+require 'rails_helper'
+
+feature "the signup process" do
+
+  it "has a new user page" do
+    visit(new_user_url)
+    expect(page).to have_content("Sign Up")
+  end
+
+  feature "signing up a user" do
+
+    let(:new_user1) { build (:user)}
+
+    before(:each) do
+      vist(new_user_url)
+      fill_in("Email", with: new_user1.email)
+      fill_in("Password", with: new_user1.password)
+      click_button("Sign Up!")
+    end
+
+
+    it "shows email on the homepage after signup"
+      expect(page).to have_content(new_user1.email)
+    end
+
+  end
+
+end
+
+feature "logging in" do
+
+  let(:new_user2) { create (:user)}
+
+  before(:each) do
+    vist(new_session_url)
+    fill_in("Email", with: new_user2.email)
+    fill_in("Password", with: new_user2.password)
+    click_button("Sign In!")
+  end
+
+  it "redirects to user's show page" do
+      expect(current_path).to match(/^\/users\/(\d)+/)
+  end
+
+  it "shows email on the homepage after login" do
+    expect(page).to have_content(new_user2.email)
+  end
+
+  it 'shows logout button on the show page' do
+    expect(page).to have_button 'Sign Out'
+  end
+
+end
+
+feature "logging out" do
+  let(:new_user3) { create (:user)}
+
+  it "begins with logged out state" do
+    visit(new_session_url)
+    expect(page).not_to have_content(new_user3.email)
+  end
+
+  it "cannot visit user show page when not logged in" do
+    visit(user_url(new_user3))
+
+    # redirect to login page
+    expect(page).to have_content("Sign In!")
+  end
+
+  before(:each) do
+    vist(new_session_url)
+    fill_in("Email", with: new_user3.email)
+    fill_in("Password", with: new_user3.password)
+    click_button("Sign In!")
+
+    click_button("Sign Out")
+  end
+
+  it "doesn't show email on the homepage after logout" do
+    expect(page).not_to have_content(new_user3.email)
+  end
+
+  it "redirect's to sign in page" do
+    expect(current_path).to match(/^\/session\/new/)
+  end
+
+end
